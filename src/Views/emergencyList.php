e4e169e8f6db1bbf413aff2ef1b3bc5eeddf593d
Alba <albamrus@gmail.com>
2022-12-02 13:21:20 +0100

    table and bootstrap adeed to views

--- src/Views/emergencyList.php
+++ src/Views/emergencyList.php
@@ -1,17 +1,47 @@
 <html>
     
+    <?php
+        require_once("Components/layout.php")
+    ?>
 
   <body>
       <main>
-        <?php
-            foreach ($data["emergency"] as $emergency) {
-                echo "{$emergency -> getName()}";
-                echo "{$emergency -> getIssue()}";
-                echo "{$emergency -> getId()}";
-                echo "{$emergency -> getClassification()}";
-                echo "{$emergency -> getDateTime()}";
-            }
-        ?>
+        <div id="wrapper">
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <div class="container-fluid card shadow mb-4 card-body">
+                    <div class="table-responsive">
+                     <table class="table table-bordered" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th scope="row">Id</th>
+                                <th>Nombre</th>
+                                <th>Emergencia</th>
+                                <th>Clasificación</th>
+                                <th>Fecha</th>
+                             </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                 foreach ($data["emergency"] as $emergency) {
+                                 echo  "
+                                 <tr>
+                                     <td>{$emergency -> getId()}</td>
+                                     <td>{$emergency -> getName()}</td>
+                                     <td>{$emergency -> getIssue()}</td>
+                                     <td>{$emergency -> getClassification()}</td>
+                                     <td>{$emergency -> getDateTime()}</td>
+                                 </tr>
+                                 ";
+                              }
+                             ?>
+                        </tbody>
+                     </table>
+                  </div>
+              </div>
+          </div>
+        </div>
+        </div>
       </main>  
     </body>
 

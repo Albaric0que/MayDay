a6d91bd70cafea16424b5114fc179d93256f01e6
ZLoln <lolanavalo@gmail.com>
2022-12-06 11:06:22 +0100

    header finished

--- src/Views/Components/header.php
+++ src/Views/Components/header.php
@@ -1,5 +1,8 @@
 <header>
-    <div>
-        
+    <div class="headerContainer">
+        <div class="headerBox">
+            <img src='./public/img/Header.png' alt="MayDay Logo">
+            <p class="headerTittle">Centro de Emergencias Anómalas</p>
+        </div>
     </div>
 </header>

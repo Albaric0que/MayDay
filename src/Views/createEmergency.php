c393e0bfa1df4ff05bf52d687cbc5c632b69a2a5
Adriana Aguilar <agruad@gmail.com>
2022-12-09 14:49:14 +0100

    buttons fixed

--- src/Views/createEmergency.php
+++ src/Views/createEmergency.php
@@ -7,30 +7,26 @@
     <main >
         <div class="createCard">
         <h2 class="cardHead">Nueva Emergencia</h2>
-             <form class="formContainer" action='?action=store' method="post">
+            <form class="formContainer" action='?action=store' method="post">
                 <div class="leftContainer">
-                <div class="input-group"  >
-                    <span class="spanName"">Nombre</span>
-                    <input  class="inputGroupName" type="text" name="name" required placeholder="Escribe aquí el nombre">
-                 </div>
-                 <div class="input-group">
+                    <div class="input-group"  >
+                        <span class="spanName"">Nombre</span>
+                        <input  class="inputGroupName" type="text" name="name" required placeholder="Escribe aquí el nombre">
+                    </div>
+                    <div class="input-group">
                         <span class="spanClassification">Clasificación</span>
                         <textarea class="inputGroupClassification" area-label="With textarea" type="text" name="classification" required placeholder="Escribe aquí la clasificación"></textarea>
-                </div>
+                    </div>
                 </div>
                 <div class="rigthContainer">
-                <div class="input-group">
-                    <span class="spanEmergency">Emergencia</span>
-                    <textarea class="inputGroupEmergency" area-label="With textarea" type="text" name="issue" required placeholder="Descripción de la emergencia"></textarea>
-                </div>
-                </div>
-                <!-- <div class="buttons" style="margin-bottom: 2rem;">
-                    <a href="?action=create"><button type="image"><img src='./public/img/save.png' alt="Botón de guardar" width=75%></button></a>
-                    <a href="./index.php"><button type="button" id="button-cancel" class="btn btn-outline-danger button-cancel"><img src='./public/img/cancel.png' alt="Botón de cancelar" width="75%"></button></a>
-                </div> -->
-                <div class="botones " style="margin-bottom: 2rem;">
-                    <input class="btn-lg btn-outline-success go-add-task float" type="image" value="Crear" src='./public/img/save.png' width="50%">
-                    <a href="./index.php"><button type="button" id="button-cancel" class="btn btn-outline-danger button-cancel">Cancel</button></a>
+                    <div class="input-group">
+                        <span class="spanEmergency">Emergencia</span>
+                        <textarea class="inputGroupEmergency" area-label="With textarea" type="text" name="issue" required placeholder="Descripción de la emergencia"></textarea>
+                        <div class="buttons">
+                            <input class="buttonCreate"type="image" value="Crear" src='./public/img/save.png' width="50%">
+                            <a href="./index.php"><button type="button" class="buttonCancel"><img src='./public/img/cancel.png' alt="Botón de cancelar" width=75%></button></a>
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
--- src/Views/editEmergency.php
+++ src/Views/editEmergency.php
@@ -3,7 +3,7 @@
     require_once("Components/header.php");
 ?>
 <body>     
-     <main>
+    <main>
         <div class="createCard">
             <h2 class="cardHead">Editar Emergencia</h2>
             <form class="formContainer" action='?action=update&id=<?php echo $data["emergency"]->getId()?>' method="post">
@@ -21,20 +21,15 @@
                     <div class="input-group">
                         <span class="spanEmergency">Emergencia</span>
                         <textarea class="inputGroupEmergency" area-label="With textarea" type="text" name="issue" required><?php echo $data["emergency"]->getIssue()?></textarea>
+                        <div class="buttons">
+                            <input class="buttonCreate"type="image" value="Crear" src='./public/img/save.png' width="50%">
+                            <a href="./index.php"><button type="button" class="buttonCancel"><img src='./public/img/cancel.png' alt="Botón de cancelar" width=75%></button></a>
+                            <input class="buttonReset" type="reset" value="" style="background-image: url(./public/img/reset.png);">
+                        </div>
                     </div>
                 </div>
-                <!-- <div class="botones">
-                    <a href="./index.php"><button type="reset" class="btn btn-outline-danger button-cancel"><img src='./public/img/reset.png' alt="Botón de reset" width=50%></button></a>
-                    <a href="./index.php"><button type="button" class="btn btn-outline-danger button-cancel"><img src='./public/img/save.png' alt="Botón de guardar" width=50%></button></a>
-                    <a href="./index.php"><button type="button" id="button-cancel" class="btn btn-outline-danger button-cancel"><img src='./public/img/cancel.png' alt="Botón de cancelar" width=50%></button></a>
-                </div> -->
-                <div class="buttons">
-                    <input class="buttonCreate"type="image" value="Crear" src='./public/img/save.png' width="50%">
-                    <a href="./index.php"><button type="button" class="buttonCancel"><img src='./public/img/cancel.png' alt="Botón de cancelar" width=50%></button></a>
-                    <input class="buttonReset" type="reset" value="" style="background-image: url(./public/img/reset.png);">
-                </div>
-                </form>
-            </div>
+            </form>
+        </div>
     </main>
 <?php
         require_once("Components/footer.php")

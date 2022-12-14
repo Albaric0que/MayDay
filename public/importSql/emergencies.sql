4b063d2c196e1fa8f6177bac1298da6f652b3c2d
Adriana Aguilar <agruad@gmail.com>
2022-12-14 16:17:59 +0100

    emergencies table sql added

--- public/importSql/emergencies.sql
+++ public/importSql/emergencies.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-12-2022 a las 20:18:15
+-- Tiempo de generación: 13-12-2022 a las 20:17:44
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
